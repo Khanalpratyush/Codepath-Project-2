0a5aa5b325a1c344f3468faa2da01502fb9cd166
Khanalpratyush <85870243+Khanalpratyush@users.noreply.github.com>
2023-03-08 21:37:56 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,135 +1,124 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import mountainImage from './images/mountains.jpeg'
+import russiaImage from './images/Russia.webp'
+import kamalaImage from './images/Kamala.webp'
+import pendriveImage from './images/pendrive.jpeg'
+import bitsImage from './images/bits.jpeg'
+import ramImage from './images/ram.jpeg'
+import printerImage from './images/printer.jpeg'
+import vacuumTubesImage from './images/vaccumtube.jpeg'
+import unImage from './images/un.jpeg'
+import ibmImage from './images/ibmimage.jpeg'
 
+const questions = [
+  {
+    text: "What is the highest peak of the world?",
+    answer: "Mount Everest",
+    image: mountainImage,
+    difficulty: 'easy'
+  },
+  {
+    text: "Which is the biggest country in the world?",
+    answer: "Russia",
+    image: russiaImage,
+    difficulty: 'easy'
+
+  },
+  {
+    text: "Who is the vice president of the USA?",
+    answer: "Kamala Harris",
+    image: kamalaImage,
+    difficulty: 'easy'
+
+  },
+  {
+    text: "Is Pendrive a storage device?",
+    answer: "Yes, it is.",
+    image: pendriveImage
+  },
+  {
+    text: "How many bits make 1 byte?",
+    answer: "8 bits",
+    image: bitsImage,
+    difficulty: 'easy'
+
+  },
+  {
+    text: "What is the full form of RAM?",
+    answer: "Random Access Memory",
+    image: ramImage,
+    difficulty: 'easy'
+
+  },
+  {
+    text: "Is Printer an example of output device?",
+    answer: "Yes, Printer is an output device",
+    image: printerImage,
+    difficulty: 'easy'
+
+  },
+  {
+    text: "Which electronic component was used in first generation of computer?",
+    answer: "Vacuum Tubes",
+    image: vacuumTubesImage,
+    difficulty: 'easy'
+
+  },
+  {
+    text: "What is the full form of UN?",
+    answer: "United Nations",
+    image: unImage,
+    difficulty: 'easy'
+
+  },
+  {
+    text: "When was IBM Computer invented?",
+    answer: "August 12, 1981",
+    image: ibmImage,
+    difficulty: 'hard'
+  }
+]
 
 const App = () => {
+  const [index, setIndex] = useState(0)
+  const [showAnswer, setShowAnswer] = useState(false)
 
-  const[index, setIndex] = useState(0);
-  
+  const toggleQuestionAnswer = (i) => {
+    setShowAnswer(!showAnswer);
+  }
+
   const updateQuestionNum = () => {
-      setIndex(Math.floor(Math.random() * (9 - 1 + 1)) + 1);
+    setIndex(Math.floor(Math.random() * questions.length))
+    setShowAnswer(false);
   }
 
-  const[qa1,setQA1] = useState("What is the highest peak of world?");
-  const[qa2,setQA2] = useState("Which is the biggest Country in the world?");
-  const[qa3,setQA3] = useState("Who is the vice president of the USA?");
-  const[qa4,setQA4] = useState("Is Pendrive a storage device?");
-  const[qa5,setQA5] = useState("How many bits make 1 byte?");
-  const[qa6,setQA6] = useState("What is the full form of RAM?");
-  const[qa7,setQA7] = useState("Is Printer an example of output device?");
-  const[qa8,setQA8] = useState("Which electronic component was used in first generation of computer?");
-  const[qa9,setQA9] = useState("What is the Full form of UN?");
-  const[qa10,setQA10] = useState("When was IBM Computer invented?");
-  
-
-
-  const updateQA1 = () => {
-    if (qa1 == "What is the highest peak of world?"){
-      setQA1("Mount Everest");
-    }
-    else{
-      setQA1("What is the highest peak of world?");
-    }
-  }
-
-  const updateQA2 = () => {
-    if (qa2 == "Which is the biggest Country in the world?"){
-      setQA2("Russia");
-    }
-    else{
-      setQA2("Which is the biggest Country in the world?");
-    }
-  }
-
-  const updateQA3 = () => {
-    if (qa3 == "Who is the vice president of the USA?"){
-      setQA3("Kamala Harris");
-    }
-    else{
-      setQA3("Who is the vice president of the USA?");
-    4
-  }
-}
-
-  const updateQA4 = () => {
-    if (qa4 == "Is Pendrive a storage device?"){
-      setQA4("Yes, It is.");
-    }
-    else{
-      setQA4("Is Pendrive a storage device?");
-    }
-  }
-
-  const updateQA5 = () => {
-    if (qa5 == "How many bits make 1 byte?"){
-      setQA5("8 bits");
-    }
-    else{
-      setQA5("How many bits make 1 byte?");
-    }
-  }
-
-  const updateQA6 = () => {
-    if (qa6 == "What is the full form of RAM?"){
-      setQA6("Random Access Memory");
-    }
-    else{
-      setQA6("What is the full form of RAM?");
-    }
-  }
-
-  const updateQA7 = () => {
-    if (qa7 == "Is Printer an example of output device?"){
-      setQA7("Yes, Printer is an output device");
-    }
-    else{
-      setQA7("Is Printer an example of output device?");
-    }
-  }
-
-  const updateQA8 = () => {
-    if (qa8 == "Which electronic component was used in first generation of computer?"){
-      setQA8("Vacuum Tubes");
-    }
-    else{
-      setQA8("Which electronic component was used in first generation of computer?");
-    }
-  }
-
-  const updateQA9 = () => {
-    if (qa9 == "What is the Full form of UN?"){
-      setQA9("United Nations");
-    }
-    else{
-      setQA9("What is the Full form of UN?");
-    }
-  }
-
-  const updateQA10 = () => {
-    if (qa10 == "When was IBM Computer invented?"){
-      setQA10("August 12, 1981");
-    }
-    else{
-      setQA10("When was IBM Computer invented?");
-    }
+  const colorDict = {
+    easy: 'green',
+    medium: 'yellow',
+    hard: 'red'
   }
 
 
-  const arrQuestion = [qa1, qa2, qa3, qa4, qa5, qa6, qa7, qa8, qa9, qa10];
-  const arrupdateAnswer = [updateQA1,updateQA2,updateQA3,updateQA4, updateQA5,updateQA6,updateQA7,updateQA8, updateQA9,updateQA10];
+  const currentQuestion = questions[index]
 
-  const arrBGColor = ["Green", "Yellow", "Red","Green", "Yellow", "Marron","Green", "Yellow", "Yellow","Red"]
-  return(
+  return (
     <div className='wholebody'>
-      <Header/>
-
-      <div className= {arrBGColor[index]} onClick={arrupdateAnswer[index]}>
-          <h1>{arrQuestion[index]}</h1>
+      <Header />
+      <div className='question-container'>
+      <div className='question-box' style={{backgroundColor: colorDict[currentQuestion.difficulty]}} onClick={() => toggleQuestionAnswer()}>
+      <img className='question-image' src={currentQuestion.image} alt='Question' />
+        {!showAnswer && 
+          <h1 className='question-text'>{currentQuestion.text}</h1>}
+        {showAnswer && 
+          <h1 className='answer-text'>{currentQuestion.answer}</h1>
+        }
+        </div>
       </div>
-      <button className = 'button' onClick={updateQuestionNum}>➡️</button>
+      <button className='button' onClick={updateQuestionNum}>➡️</button>
     </div>
   )
 }
 
-export default App;
+export default App
